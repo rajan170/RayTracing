08114ce3efa9ac614f27a4ee8e34d8eec9435327
Rajan <rajan@Andromeda.local>
2024-11-27 15:05:20 -0500

    main updated

--- main.cc
+++ main.cc
@@ -1,3 +1,6 @@
+#include "color.h"
+#include "vec3.h"
+
 #include <iostream>
 
 int main(){
@@ -5,20 +8,14 @@ int main(){
     int img_height = 256;
 
     std::cout<<"P3\n"<<img_width<<" "<<img_height<<"\n255\n";
+    
     for(int j=0; j<img_height; j++){
         std::clog << "\rScanLines remaining: " << (img_height - j) << " " << std::flush;
         for(int i=0; i<img_width; i++){
-            auto r = double(i) / (img_width-1);
-            auto g = double(j) / (img_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 *r);
-            int ig = int(255.999 *g);
-            int ib = int(255.999 *b);
-            
-            std::cout<< ir << " " << ig << " " << " " << ib << '\n';
+            auto pixel_color = color(double(i)/(img_width-1), double(j)/(img_width-1), 0);
+            write_color(std::cout, pixel_color);
         }
     }
-    std::clog<< "\rDone.   \n";
+    std::clog<< "\rDone.       \n";
     return 0;
 }
